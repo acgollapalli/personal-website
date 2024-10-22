973d5f0ba967ba8054eb3744c617b47acfc3d710
Akshay Gollapalli <akshaygollapalli@Akshays-MacBook-Pro.local>
2024-10-22 02:01:00 -0500

    blog shadcn and next15

---
+++ app/sitemap.ts
@@ -0,0 +1,20 @@
+import { getBlogPosts } from './blog/utils'
+
+export const baseUrl = 'http://localhost:3000'
+
+// FIXME: this needs to be possibly dynamic?
+// but only if the blogs become dynamic. If they
+// remain static in repo, then it's no issue
+export default async function sitemap() {
+  let blogs = getBlogPosts().map((post) => ({
+    url: `${baseUrl}/blog/${post.slug}`,
+    lastModified: post.metadata.publishedAt,
+  }))
+
+  let routes = ['', '/blog'].map((route) => ({
+    url: `${baseUrl}${route}`,
+    lastModified: new Date().toISOString().split('T')[0],
+  }))
+
+  return [...routes, ...blogs]
+}
