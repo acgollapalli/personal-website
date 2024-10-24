76c8330b8d661e4e66cf07be87d9c9b367ccdc7b
Akshay C. Gollapalli <acgollapalli@jnasquare.com>
2024-10-24 15:37:45 -0500

    TODO: start an x account under my actual name and then post on it exclusively through commit messages to this repo

--- app/(main)/about/me/page.tsx
+++ app/(main)/about/me/page.tsx
@@ -5,6 +5,8 @@ import { Separator } from "@/components/ui/separator"
 
 import { AboutMeHeader } from "@/components/headings/about-me";
 
+// TODO: start an x account under my actual name
+// and then post on it exclusively through commit messages to this repo
 export default function AboutMe() {
   return (
     <>
@@ -12,7 +14,7 @@ export default function AboutMe() {
         <div className="w-full max-w-7xl mx-auto">
           <AboutMeHeader/>
         </div>
-    <main className="flex flex-col gap-8 py-16">
+      <main className="flex flex-col gap-8 py-16">
 
     </main>
 
